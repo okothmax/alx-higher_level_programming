789e05a1a292b666aca05f977f60eeaccc9151b1
okothmax <okothmaxwell03@gmail.com>
2023-02-09 13:43:24 +0300

    almost circle task one

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/bin/bash/python3
+"""
+creating the Base class
+"""
+
+class Base:
+    '''
+    private class attributes
+    
+    nb_objects'''
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
