9530333e392da71ca4dc42fc48386522ce487b2a
Orain <112311293+okothmax@users.noreply.github.com>
2023-04-11 13:00:26 +0000

    fixed javascript error

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (const entry of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+for (const entry of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
   console.log(entry);
 }
