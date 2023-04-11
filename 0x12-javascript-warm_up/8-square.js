d62c710010ab36f2c9491655245292f357e22327
Orain <112311293+okothmax@users.noreply.github.com>
2023-04-11 13:05:24 +0000

    update

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,12 +3,9 @@ const size = Math.floor(Number(process.argv[2]));
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let width = 0; width < size; width++) {
-    let joiner = '';
-    for (let length = 0; length < size; length++) 
-    {
-      joiner += 'X';
-    }
-    console.log(joiner);
+  for (let r = 0; r < size; r++) {
+    let row = '';
+    for (let c = 0; c < size; c++) row += 'X';
+    console.log(row);
   }
 }
