f329f3449269e9fc566f9b1e067f095f87b33b06
Orain <112311293+okothmax@users.noreply.github.com>
2023-04-16 18:01:31 +0000

    imported Base

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -10,7 +10,7 @@ Execution: ./13-model_state_delete_a.py <mysql username> /
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import State, Base
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
